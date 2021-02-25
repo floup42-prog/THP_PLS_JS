11444f0eb4ba65015068d1160644f5c7843c8ef6
floup42 <pousseurdec@gmail.com>
2021-02-25 19:46:37 +0100

    first commit

---
+++ script_1.js
@@ -0,0 +1,8 @@
+console.log("Bonjour monde");
+
+
+let hello_word = "Bonjour ";
+
+prenom = prompt("Quelle prénom");
+console.log(hello_word += prenom);
+console.log("Comment ça va ?");
---
+++ script_2.js
@@ -0,0 +1,12 @@
+nbr = prompt("Choisi un nombre ?");
+Number(nbr);
+
+function factorielle(){
+    for (let i = nbr-1; i != 0; i--) {
+        // console.log(i);
+        nbr = (nbr*i);
+        console.log(nbr);
+    }
+}
+
+factorielle();
